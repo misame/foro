f996f351497ef0b0f639c90a15956afa21792315
misame <35807643+misame@users.noreply.github.com>
2023-05-11 17:14:03 -0500

    Se agrega la clase DatabaseConfig para manejar la proteccion del usuario y contraseña de la base de datos

---
+++ src/main/java/com/alura/api/infra/DatabaseConfig.java
@@ -0,0 +1,25 @@
+package com.alura.api.infra;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
+
+public class DatabaseConfig {
+    @Value("${spring.datasource.url}")
+    private String dbUrl;
+    @Value("${spring.datasource.username}")
+    private String dbUsername;
+    @Value("${spring.datasource.password}")
+    private String dbPassword;
+    @Bean
+    public DataSource dataSource(){
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+        dataSource.setUrl(dbUrl);
+        dataSource.setUsername(dbUsername);
+        dataSource.setPassword(dbPassword);
+        return dataSource;
+    }
+}
